38fc9b4792674777634e38674bc3cfb7cba48fbb
dmitrypashkov <dmitry.pashkov@nextiva.com>
2017-04-09 13:20:43 +0300

    Second part of data types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,36 +1,58 @@
-$("#main").append("Dmitry Pashkov");
-var awesomeThoughts = "I am Dmitry Pashkov and I am awesome!"
-var funThoughts = "I am Dmitry Pashkov and I am awesome!".replace("awesome", "fun");
-console.log(awesomeThoughts);
-console.log(funThoughts);
-var htmlHeaderName = HTMLheaderName.replace("%data%", "Dmitry Pashkov");
-var htmlHeaderRole = HTMLheaderRole.replace("%data%", "Java Developer");
-$("#header").prepend(htmlHeaderRole);
-$("#header").prepend(htmlHeaderName);
+var bio = {
+	 "name": "Dmitry Pashkov",
+	 "role": "Java Developer",
+	 "contantInfo": {
+	 	"generic" : "Ukraine, Kiev",
+	 	"mobile": "My Mobile",
+	 	"email": "My Email",
+	 	"twitter": "My twitter",
+	 	"github": "My github",
+	 	"blog": "My blog",
+	 	"location":"My location"
+	 },
+	 "skills": ["Java", "JavaScript", "HTMl"],
+	 "pic": "images/me.jpg",
+	 "welcomeMessage": "My welcome message"
+}; 
 
-var s = "audacity";
+var work = {};
+work.employer = "Ciklum";
+work.title = "Java Developer";
+work.dates = "2012-2016";
+work.location = "Ukraine, Odessa";
+work.description = "Developing a Spring Based project";
 
-var udacityizer = function(s) {  
-    // Right now, the variable s === "audacity"
-    // Manipulate s to make it equal to "Udacity"
-    // Your code goes here!
-    
-    s = s.substring(1, 2).toUpperCase()+s.substring(2);
-    return s;
-};
+var education = {};
+education["name"] = "My school";
+education["degree"] = "My degree";
+education["dates"] = "2012-2016";
+education["location"] = "My school location";
+education["major"] = "My school major";
 
-// Did your code work? The line below will tell you!
-console.log(udacityizer(s));
+$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+$("#topContacts").append(HTMLcontactGeneric.replace("%contact%", "Generic data").replace("%data%", bio.contantInfo.generic));
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contantInfo.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contantInfo.email));
+$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contantInfo.twitter));
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contantInfo.github));
+$("#topContacts").append(HTMLblog.replace("%data%", bio.contantInfo.blog));
+$("#topContacts").append(HTMLlocation.replace("%data%", bio.contantInfo.location));
+$("#topContacts").append(HTMLskillsStart);
+$("#topContacts").append(HTMLskills.replace("%data%", bio.skills));
+$("#topContacts").append(HTMLbioPic.replace("%data%", bio.pic));
+$("#topContacts").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
 
-var arrayOfNumbers = [1,2,3,4,5];
-var lastNumber = arrayOfNumbers.pop();
-lastNumber++;
-arrayOfNumbers.push(lastNumber);
-console.log(arrayOfNumbers);
+$("#workExperience").append(HTMLworkStart);
+$("#workExperience").append(HTMLworkEmployer.replace("%data%", work.employer));
+$("#workExperience").append(HTMLworkTitle.replace("%data%", work.title));
+$("#workExperience").append(HTMLworkDates.replace("%data%", work.dates));
+$("#workExperience").append(HTMLworkLocation.replace("%data%", work.location));
+$("#workExperience").append(HTMLworkDescription.replace("%data%", work.description));
 
-var name = "cAmEROn PittMAN";
-var names = name.split(" ");
-names[0] = names[0].slice(0,1).toUpperCase()+names[0].slice(1).toLowerCase();
-names[1] = names[1].toUpperCase();
-name = names[0]+" "+names[1];
-console.log(names);
+$("#education").append(HTMLschoolStart);
+$("#education").append(HTMLschoolName.replace("%data%", education.name));
+$("#education").append(HTMLschoolName.replace("%data%", education.degree));
+$("#education").append(HTMLschoolName.replace("%data%", education.dates));
+$("#education").append(HTMLschoolName.replace("%data%", education.location));
+$("#education").append(HTMLschoolName.replace("%data%", education.major));
