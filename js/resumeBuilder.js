2221814dbe33cb637ee02c31223b9cec78c88b55
dmitrypashkov <dmitry.pashkov@nextiva.com>
2017-04-09 12:39:58 +0300

    Quizes from Data Types section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,36 @@
 $("#main").append("Dmitry Pashkov");
+var awesomeThoughts = "I am Dmitry Pashkov and I am awesome!"
+var funThoughts = "I am Dmitry Pashkov and I am awesome!".replace("awesome", "fun");
+console.log(awesomeThoughts);
+console.log(funThoughts);
+var htmlHeaderName = HTMLheaderName.replace("%data%", "Dmitry Pashkov");
+var htmlHeaderRole = HTMLheaderRole.replace("%data%", "Java Developer");
+$("#header").prepend(htmlHeaderRole);
+$("#header").prepend(htmlHeaderName);
+
+var s = "audacity";
+
+var udacityizer = function(s) {  
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+    
+    s = s.substring(1, 2).toUpperCase()+s.substring(2);
+    return s;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
+
+var arrayOfNumbers = [1,2,3,4,5];
+var lastNumber = arrayOfNumbers.pop();
+lastNumber++;
+arrayOfNumbers.push(lastNumber);
+console.log(arrayOfNumbers);
+
+var name = "cAmEROn PittMAN";
+var names = name.split(" ");
+names[0] = names[0].slice(0,1).toUpperCase()+names[0].slice(1).toLowerCase();
+names[1] = names[1].toUpperCase();
+name = names[0]+" "+names[1];
+console.log(names);
